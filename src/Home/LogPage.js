57c073cabc2766a80986f1f4f57c8921de2e9243
Epixylium <michael.h.hibbert@gmail.com>
2020-08-04 12:03:21 +0100

    Things were done Mistakes were made xD
    - Started making the start/stop button work (works on company1 for now but nothing else)
    
    - uploaded my first data thing to the blockchain :D

--- src/Home/LogPage.js
+++ src/Home/LogPage.js
@@ -3,73 +3,88 @@ import Timetable from '../components/Timesheet/TimeTable';
 
 class LogPage extends Component {
     state = {
-      buttonState: 'start'
+      buttonState: "start",
+      timelogs: {
+        company1: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"work",
+        }],
+
+        company2: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"more work",
+        }],
+                  
+        company3: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"even more work",
+        }],
+
+        company4: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"even more work still",
+        }],
+                
+        company5: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"wow thats a lot of work",
+        }],
+
+        company6: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"work is my life",
+        }],
+      
+        company7: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"work is a thing i do",
+        }],
+
+        company8: [{
+          start:new Date(),
+          finish:new Date(),
+          desc:"woooooorrrrrk",
+        }],
+      }
+
     }
 
-   // changeButtonState() {
-     // if(buttonState = "start") {
-     //   this.setState.buttonState = "finish"
-    //  }
-   // }
+    changeButtonState() {
+      if(this.state.buttonState = "start") {
+        const timelogs = {...this.state.timelogs};
+        const company = timelogs['company1'];
+
+        company.push(this.createTimeLog());
+
+        timelogs['company1'] = company;
+
+        this.setState({buttonState: "finish", timelogs: timelogs }); 
+      }
+    }
+
+    createTimeLog() {
+      return {
+        start:new Date(),
+        finish:new Date(),
+        desc:"work is a thing i do",
+      }
+    }
 
     render() {
-        const timelogs = {
-              company1: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"work",
-              }],
-
-              company2: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"more work",
-              }],
-                        
-              company3: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"even more work",
-              }],
-
-              company4: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"even more work still",
-              }],
-                      
-              company5: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"wow thats a lot of work",
-              }],
-
-              company6: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"work is my life",
-              }],
-            
-              company7: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"work is a thing i do",
-              }],
-
-              company8: [{
-                start:new Date(),
-                finish:new Date(),
-                desc:"work is a like a skateboard... i just cant stay on it xD",
-              }],
-            };
-
         return(
             <> 
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                  <Timetable timelogs={timelogs}></Timetable>
+                  <Timetable timelogs={this.state.timelogs}></Timetable>
                 </div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                  <button onClick={this.changeButtonState}>Start/Stop Time</button>
+                  <button onClick={this.changeButtonState.bind(this)}>Start/Stop Time</button>
                 </div>
             </>
         );
--- tests/test-arweave.js
+++ tests/test-arweave.js
@@ -27,3 +27,64 @@ arweave.wallets.jwkToAddress(jwk).then((wallet_address) => {
         // eg 0.125213858712
     });
 });
+
+async function createTransactions() {
+    // Plain text
+    let transactionA = await arweave.createTransaction({
+        data: '<html><head><meta charset="UTF-8"><title>Hello world!</title></head><body></body></html>'
+    }, jwk);
+
+    transactionA.addTag("AppName", "TimeLord");
+
+    await arweave.transactions.sign(transactionA, jwk);
+    await arweave.transactions.post(transactionA);
+
+    console.log(transactionA);
+}
+
+
+
+arweave.arql({
+    op: "and",
+    expr1: {
+      op: "equals",
+      expr1: "from",
+      expr2: "6b5e6Ys64SNOVJQ396ewkrkL4VQ5sBTFOT8-QXxCgNE"
+    },
+    expr2: {
+      op: "equals",
+      expr1: "AppName",
+      expr2: "TimeLord"
+    }
+  }).then(txids => {
+    debugger;
+    for(let i in txids) {
+        let tx = arweave.transactions.get(txids[i]).then(transaction => {
+            console.log(transaction);
+        });
+    
+        tx = arweave.transactions.getData(txids[i], {decode: true, string: true}).then(data => {
+            console.log(data);
+        });
+    }
+  })
+
+
+
+//createTransactions();
+
+// Transaction {
+//   format: 2,
+//   id: 'ReUohI9tEmXQ6EN9H9IkRjY9bSdgql_OdLUCOeMEte0',
+//   last_tx: 'Tk-0c7260Ya5zjfjzl4f6-W-vRO94qiqZMAScKBcYXc68v1Pd8bYfTbKWi7pepUF',
+//   owner: 'kmM4O08BJB85RbxfQ2nkka9VNO6Czm2Tc_IGQNYCTSXRzO...',
+//   tags: [],
+//   target: '',
+//   quantity: '0',
+//   data: 'c29tZSBkYXRh',
+//   data_size: '9',
+//   data_root: 'qwKZUl7qWpCEmB3cpONKTYOcSmnmhb-_s8ggMTZwCU4',
+//   data_tree: [],
+//   reward: '7489274',
+//   signature: 'JYdFPblDuT95ky7_wVss3Ax9e4Qygcd_lEcB07sDPUD_wNslOk...'
+// }
