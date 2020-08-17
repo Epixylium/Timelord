6737da8a9950e135a25cf223ced8a3972108be51
Epixylium <michael.h.hibbert@gmail.com>
2020-08-17 11:57:39 +0100

    Not my best work session
    didnt get a ton done like i would have prefered but started tidying some code up and added a helpers.js file to house all the functions im gonna be making...

--- src/Home/ClientsPage.js
+++ src/Home/ClientsPage.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
+import {saveClient} from './helpers';
 
 class ClientsPage extends Component {
+    state = {
+        Cid: "",
+        Cname: "",
+        Caddress: "",
+    }
+
+    onChange(event) {
+        const name = event.target.name;
+        const newValue = {};
+
+        newValue[name] = event.target.value;
+
+        this.setState(newValue);
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        
+        saveClient(this.state.Cid, this.state.Cname, this.state.Caddress);
+    }
+
     render() {
         return (
             <form className="form-horizontal" style={{paddingLeft: "750px", paddingRight: "750px"}}>
                 <div className="form-group">
                     <label className="control-label col-sm-2" htmlFor="Cid">Company ID</label>
-                    <input className="form-control" type="text" id="Cid" name="Cid"></input>
+                    <input onChange={ (event) => { this.onChange(event) }} className="form-control" type="text" id="Cid" name="Cid"></input>
                 </div>
                 <div className="form-group">
                     <label className="control-label col-sm-2" htmlFor="Cname">Company Name</label>
-                    <input className="form-control" type="text" id="Cname" name="Cname"></input>
+                    <input onChange={ (event) => { this.onChange(event) }} className="form-control" type="text" id="Cname" name="Cname"></input>
                 </div>
                 <div className="form-group">
                     <label className="control-label col-sm-2" htmlFor="Caddress">Company Address</label>
-                    <input className="form-control" type="text" id="Caddress" name="Caddress"></input>
+                    <input onChange={ (event) => { this.onChange(event) }} className="form-control" type="text" id="Caddress" name="Caddress"></input>
                 </div>
                 <div className="form-group">
-                    <input type="submit" value="Submit"/>
+                    <input onClick={ (event) => { this.onSubmit(event) }} type="submit" value="Submit"/>
                 </div>
             </form>
         )
---
+++ src/Home/helpers.js
@@ -0,0 +1,5 @@
+export function saveClient(client_id, client_name, client_address) {
+    console.log(client_id + " " + client_name + " " + client_address);
+
+    
+}
